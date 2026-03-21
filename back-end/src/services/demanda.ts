9ecb04a68a7088e9508da2621cb2eeccb76828a7
Gabriel Balastrelli <gbalastrelli@gmail.com>
2026-03-21 19:04:49 -0300

    Feat/Criar findAll e findId para demanda

--- back-end/src/services/demanda.ts
+++ back-end/src/services/demanda.ts
@@ -1,4 +1,49 @@
-import { PrismaClient } from '@prisma/client';
+import { Demanda, PrismaClient } from '@prisma/client';
 import { DemandaData, IDemandaRepo } from '../interfaces/interfaceDemanda';
 
-export class Demanda 
+const prisma = new PrismaClient();
+
+export class UseDemanda implements IDemandaRepo {
+  async create(data: DemandaData | Omit<Demanda, 'id_demanda'>): Promise<Demanda> {
+    return await prisma.demanda.create({
+      data: {
+        id_projeto: data.id_projeto,
+        id_empregado: data.id_empregado,
+        ds_nome: data.ds_nome,
+        ds_descricao: data.ds_descricao,
+        dt_inicio: data.dt_inicio,
+        dt_fim: data.dt_fim,
+      },
+    });
+  }
+
+  async update(idDemanda: string, data: Partial<DemandaData>): Promise<Demanda> {
+    return await prisma.demanda.update({
+      where: { id_demanda: idDemanda },
+      data: {
+        id_projeto: data.id_projeto,
+        id_empregado: data.id_empregado,
+        ds_nome: data.ds_nome,
+        ds_descricao: data.ds_descricao,
+        dt_inicio: data.dt_inicio,
+        dt_fim: data.dt_fim,
+      },
+    });
+  }
+
+  async delete(idDemanda: string): Promise<void> {
+    await prisma.demanda.delete({
+      where: { id_demanda: idDemanda },
+    });
+  }
+
+  async findAll(): Promise<Demanda[]> {
+    return await prisma.demanda.findMany();
+  }
+
+  async findId(idDemanda: string): Promise<Demanda | null> {
+    return await prisma.demanda.findUnique({
+      where: { id_demanda: idDemanda },
+    });
+  }
+}
