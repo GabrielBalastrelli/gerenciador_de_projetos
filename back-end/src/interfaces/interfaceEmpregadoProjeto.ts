d1920288c4d24143af6dfced97af2c15db6755d9
Gabriel Balastrelli <gbalastrelli@gmail.com>
2026-03-25 20:40:44 -0300

    Feat/Criar post, put e delete

--- back-end/src/interfaces/interfaceEmpregadoProjeto.ts
+++ back-end/src/interfaces/interfaceEmpregadoProjeto.ts
@@ -1,13 +1,16 @@
 import { EmpregadoProjeto } from '@prisma/client';
 
 export type EmpregadoProjetoData = {
-  id_empregado: String;
-  id_projeto: String;
+  id_empregado: string;
+  id_projeto: string;
 };
 
 export interface IEmpregadoProjetoRepo {
   create(data: EmpregadoProjetoData | Omit<EmpregadoProjeto, 'id'>): Promise<EmpregadoProjeto>;
-  update(data: EmpregadoProjetoData | Partial<EmpregadoProjeto>): Promise<EmpregadoProjeto>;
+  update(
+    id: string,
+    data: EmpregadoProjetoData | Partial<EmpregadoProjeto>,
+  ): Promise<EmpregadoProjeto>;
   delete(id: string): Promise<void>;
   findAll(): Promise<EmpregadoProjeto[]>;
   findId(id: string): Promise<EmpregadoProjeto | null>;
