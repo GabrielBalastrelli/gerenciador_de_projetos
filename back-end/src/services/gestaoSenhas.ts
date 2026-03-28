8fbc58d52a095440bdf95f70d6eaf9c9d365c357
Gabriel Balastrelli <gbalastrelli@gmail.com>
2026-03-28 14:58:27 -0300

    Fix/Padronizaçcão do nome dos parametros

--- back-end/src/services/gestaoSenhas.ts
+++ back-end/src/services/gestaoSenhas.ts
@@ -2,15 +2,15 @@ import bcrypt from 'bcrypt';
 import { IGestaoSenha } from '../interfaces/interfaceGestaoSenha';
 
 export class GestaoSenha implements IGestaoSenha {
-  async criptografarSenha(password: string): Promise<string> {
+  async criptografarSenha(senha: string): Promise<string> {
     const salt = 10;
 
-    const hash = await bcrypt.hash(password, salt);
+    const hash = await bcrypt.hash(senha, salt);
 
     return hash;
   }
 
-  async compararSenha(password: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(password, hash);
+  async compararSenha(senha: string, senhaLogin: string): Promise<boolean> {
+    return await bcrypt.compare(senha, senhaLogin);
   }
 }
