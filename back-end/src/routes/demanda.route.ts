247414bf07a7cebd922fdb4205aeb7b01148b9b6
Gabriel Balastrelli <gbalastrelli@gmail.com>
2026-04-17 20:49:51 -0300

    Conscertando commit 1

--- back-end/src/routes/demanda.route.ts
+++ back-end/src/routes/demanda.route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import ControllerDemanda from '../controllers/demanda.controller';
 import { ControllerAuth } from '../controllers/auth.controller';
+import cors from 'cors';
 
 const demandaRouter = Router();
 const controller = new ControllerDemanda();
 const authController = new ControllerAuth();
+
+demandaRouter.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 
 demandaRouter.use(authController.middlewareValidaToken.bind(authController));
 demandaRouter.post('/', controller.create.bind(controller));
--- back-end/src/routes/empregado.route.ts
+++ back-end/src/routes/empregado.route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import ControllerEmpregado from '../controllers/empregado.controller';
 import { ControllerAuth } from '../controllers/auth.controller';
+import cors from 'cors';
 
 const empregadoRouter = Router();
 const controller = new ControllerEmpregado();
 const authController = new ControllerAuth();
+
+empregadoRouter.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 
 empregadoRouter.post('/', controller.create.bind(controller));
 
