b5277b05666716ed0c61834321c044b34e479157
Gabriel Balastrelli <100177322+GabrielBalastrelli@users.noreply.github.com>
2026-04-27 22:09:41 -0300

    Update auth.route.ts

--- back-end/src/routes/auth.route.ts
+++ back-end/src/routes/auth.route.ts
@@ -5,15 +5,7 @@ import cors from 'cors';
 const authRouter = Router();
 const controler = new ControllerAuth();
 
-authRouter.use(
-  cors({
-     origin: [
-      'http://localhost:5173',
-      'https://gerenciador-de-projetos-1-5b64.onrender.com'
-    ],
-    credentials: true,
-  }),
-);
+ 
 
 authRouter.post('/', controler.login.bind(controler));
 
