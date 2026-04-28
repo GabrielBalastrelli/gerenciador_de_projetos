f0b2d1420a52869daa8c75220b6ec0ab634b9be6
Gabriel Balastrelli <100177322+GabrielBalastrelli@users.noreply.github.com>
2026-04-27 21:47:35 -0300

    Update empregado.route.ts

--- back-end/src/routes/empregado.route.ts
+++ back-end/src/routes/empregado.route.ts
@@ -9,7 +9,7 @@ const authController = new ControllerAuth();
 
 empregadoRouter.use(
   cors({
-    origin: 'https://gerenciador-de-projetos-1-5b64.onrender.com',
+    origin: 'https://gerenciador-de-projetos-jjum.onrender.com',
     credentials: true,
   }),
 );
