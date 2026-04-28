5447c7b43cd7aa512dc9e042c3ca5e268c93f258
Gabriel Balastrelli <100177322+GabrielBalastrelli@users.noreply.github.com>
2026-04-27 22:09:23 -0300

    Update empregado.route.ts

--- back-end/src/routes/empregado.route.ts
+++ back-end/src/routes/empregado.route.ts
@@ -7,15 +7,7 @@ const empregadoRouter = Router();
 const controller = new ControllerEmpregado();
 const authController = new ControllerAuth();
 
-empregadoRouter.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'https://gerenciador-de-projetos-1-5b64.onrender.com'
-    ],
-    credentials: true,
-  }),
-);
+ 
 
 empregadoRouter.post('/', controller.create.bind(controller));
 
